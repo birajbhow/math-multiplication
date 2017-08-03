8622099c0c9d65eef63337e6bc7c14fcc5c7fc5e
birajbhow <birajbhow@yahoo.com>
2017-08-03 10:02:22 +0100

    added the vs solution

---
+++ MultiplicationWithoutOperator/MultiplicationWithoutOperator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationWithoutOperator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("First number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = multiply(num1, num2);
+            Console.WriteLine($"{num1} X {num2} = {result}");
+            Console.ReadKey();
+        }
+
+        public static int multiply(int num1, int num2)
+        {
+            int result = 0;
+            for(var i = 0; i < num2; i++)
+            {
+                result += num1;
+            }
+            return result;
+        }
+    }
+}
---
+++ MultiplicationWithoutOperator/UnitTests/MultiplyTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiplicationWithoutOperator;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MultiplyTests
+    {
+        [TestMethod]
+        public void multiply_method_returns_correct_result()
+        {
+            // arrange
+            int num1 = 5, num2 = 5;
+
+            // act
+            int result = Program.multiply(num1, num2);
+
+            // assert
+            Assert.AreEqual(25, result);
+        }
+    }
+}
